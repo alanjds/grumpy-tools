ca4ccc02b6b26e7ec70cfbde75d4bf5dece5a5de
Alan Justino <alan.justino@yahoo.com.br>
2018-02-18 22:02:14 -0300

    grumprun -> grumpy run

--- grumpy/cli.py
+++ grumpy/cli.py
@@ -4,15 +4,12 @@
 
 import click
 
-import grumpc
+from . import grumpc, grumprun
 
 
 @click.group('grumpy')
 def main(args=None):
     """Console script for grumpy_tools."""
-    click.echo("Replace this message by putting your code into "
-               "grumpy_tools.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
     return 0
 
 @main.command('transpile')
@@ -21,6 +18,14 @@ def transpile(args=None, script=None):
     import sys
     sys.argv.pop(0)
     return grumpc.main(grumpc.parser.parse_args())
+
+
+@main.command('run')
+@click.option('-m', '--modname', help='Run the named module')
+def transpile(args=None, modname=None):
+    import sys
+    sys.argv.pop(0)
+    return grumprun.main(grumprun.parser.parse_args())
 
 
 if __name__ == "__main__":
